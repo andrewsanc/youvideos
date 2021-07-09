ba13ad2f7f51571061719d0acbdaa5790d5cfcfe
TB831 <druu831@gmail.com>
2021-07-09 14:47:15 -0700

    added semantic ui stylesheet. added input handler and form handler.

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: '',
+        }
+    }
+
+    onInputChange = (e) => {
+        this.setState({ term: e.target.value })
+    }
+
+    onFormSubmit = (e) => {
+        e.preventDefault();
+
+        // Todo make sure call callback from parent component
+    }
 
     render() {
-        return <div>Search Bar!</div>
+        return(
+            <div className='searchBar ui segment'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
+                    <div className='field'>
+                        <label>Video Search</label>
+                        <input type='text' onChange={this.onInputChange} value={this.state.term} />
+                    </div>
+                </form>
+            </div>
+        )
     }
 }
 

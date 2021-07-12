8036c1f7b3626faf833494a103ad51e1a63b41d0
TB831 <druu831@gmail.com>
2021-07-12 08:58:41 -0700

    added apiKey to .env file

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import './App.css';
 
+console.log(process.env.REACT_APP_YOUTUBE_API_KEY)
+
 class App extends React.Component {
   
   render() {
+
     return (
-      <div>
+      <div className='ui container'>
         <SearchBar />
       </div>
     )

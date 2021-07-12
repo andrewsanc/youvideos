bc7f34bb25c7d16cfc8479f9322173a1a32fc62e
TB831 <druu831@gmail.com>
2021-07-12 10:46:43 -0700

    fixed minor react bugs. Changed VideoDetail to render iframe video player

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -6,7 +6,7 @@ const VideoItem = ({ videoObj, onVideoSelect }) => {
 
     return (
         <div onClick={() => onVideoSelect(videoObj)} className='video-item item'>
-            <img className='ui image' src={videoObj.snippet.thumbnails.medium.url} alt="Video Thumbnail" />
+            <img className='ui image' src={videoObj.snippet.thumbnails.medium.url} alt="Thumbnail {{videoObj.snippet.title}}" />
             <div className='content'>
                 <div className='header'>{videoObj.snippet.title}</div>
             </div>
--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -3,7 +3,7 @@ import VideoItem from './VideoItem';
 
 const VideoList = ({ videos, onVideoSelect }) => {
     const renderedList = videos.map(videoObj => {
-        return <VideoItem onVideoSelect={onVideoSelect} videoObj={videoObj} />;
+        return <VideoItem key={videoObj.id.videoId} onVideoSelect={onVideoSelect} videoObj={videoObj} />;
     });
 
     return (

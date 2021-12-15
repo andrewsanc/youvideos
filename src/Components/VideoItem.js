3044482651665cf3615914acd7d757dd37d813e1
andrew <druu831@gmail.com>
2021-12-14 18:23:52 -0800

    code format on functional components

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import './VideoItem.css';
+import React from "react";
+import "./VideoItem.css";
 
 const VideoItem = ({ videoObj, onVideoSelect }) => {
-    if (videoObj.hasOwnProperty('snippet')) {
-        return (
-            <div onClick={() => onVideoSelect(videoObj)} className='video-item item'>
-                <img className='ui image' src={videoObj.snippet.thumbnails.medium.url} alt="Thumbnail {{videoObj.snippet.title}}" />
-                <div className='content'>
-                    <div className='header'>{videoObj.snippet.title}</div>
-                </div>
-            </div>
-        );
-    }
+  if (videoObj.hasOwnProperty("snippet")) {
+    return (
+      <div onClick={() => onVideoSelect(videoObj)} className='video-item item'>
+        <img
+          className='ui image'
+          src={videoObj.snippet.thumbnails.medium.url}
+          alt={`Thumbnail ${videoObj.snippet.title}`}
+        />
+        <div className='content'>
+          <div className='header'>{videoObj.snippet.title}</div>
+        </div>
+      </div>
+    );
+  }
 
-    return null;
-}
+  return null;
+};
 
 export default VideoItem;

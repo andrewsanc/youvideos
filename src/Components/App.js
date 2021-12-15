f5aefae117945856670886b5e2545188d61e09ca
andrew <druu831@gmail.com>
2021-12-14 18:19:13 -0800

    change SearchBar class component to react hook component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,87 +1,91 @@
-import React from 'react';
-import SearchBar from './SearchBar';
-import VideoList from './VideoList';
-import VideoDetail from './VideoDetail';
-import youtube from '../APIs/youtube';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import SearchBar from "./SearchBar";
+import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
+import youtube from "../APIs/youtube";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       videos: [],
       selectedVideo: null,
-    }
+    };
   }
 
   async componentDidMount() {
-    const response = await youtube.get('/videos', {
+    const response = await youtube.get("/videos", {
       params: {
-        part: 'snippet',
-        chart: 'mostPopular'
-      }
+        part: "snippet",
+        chart: "mostPopular",
+      },
     });
 
-    this.setState({ 
-      videos: response.data.items.slice(1), 
-      selectedVideo: response.data.items[0], 
+    this.setState({
+      videos: response.data.items.slice(1),
+      selectedVideo: response.data.items[0],
     });
   }
 
   // Makes request to YouTubeAPI and set state to response
-  onTermSubmit = async (term) => {
-    const response = await youtube.get('/search', {
+  fetchYoutubeVideos = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
-        part: 'snippet',
+        part: "snippet",
         q: term,
-        type: 'video',
-      }
+        type: "video",
+      },
     });
 
-    this.setState({ 
-      videos: response.data.items, 
-      selectedVideo: response.data.items[0] 
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
     });
-  }
+  };
 
   onVideoSelect = (selectedVideo) => {
     this.setState({ selectedVideo });
 
-    const videoId = selectedVideo.id.videoId ? selectedVideo.id.videoId : selectedVideo.id;
+    const videoId = selectedVideo.id.videoId
+      ? selectedVideo.id.videoId
+      : selectedVideo.id;
     this.retrieveRelatedVideos(videoId);
-  }
+  };
 
   retrieveRelatedVideos = async (videoId) => {
-    const response = await youtube.get('/search', {
+    const response = await youtube.get("/search", {
       params: {
-        part: 'snippet',
+        part: "snippet",
         relatedToVideoId: videoId,
-        type: 'video'
-      }
+        type: "video",
+      },
     });
-    
+
     this.setState({
       videos: response.data.items.slice(0, 5),
     });
-  }
-  
-  render() {
+  };
 
+  render() {
     return (
       <div className='ui container'>
-        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <SearchBar fetchYoutubeVideos={this.fetchYoutubeVideos} />
         <div className='ui grid'>
           <div className='ui row'>
             <div className='eleven wide column'>
               <VideoDetail video={this.state.selectedVideo} />
             </div>
             <div className='five wide column'>
-              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,34 +1,27 @@
-import React from 'react';
+import React, { useState } from "react";
 
-class SearchBar extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            term: '',
-        }
-    }
+const SearchBar = ({ fetchYoutubeVideos }) => {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    onInputChange = (e) => {
-        this.setState({ term: e.target.value })
-    }
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    fetchYoutubeVideos(searchTerm);
+  };
 
-    onFormSubmit = (e) => {
-        e.preventDefault();
-        this.props.onFormSubmit(this.state.term);
-    }
-
-    render() {
-        return(
-            <div className='searchBar ui segment'>
-                <form onSubmit={this.onFormSubmit} className='ui form'>
-                    <div className='field'>
-                        <label>Video Search</label>
-                        <input type='text' onChange={this.onInputChange} value={this.state.term} />
-                    </div>
-                </form>
-            </div>
-        )
-    }
-}
+  return (
+    <div className='searchBar ui segment'>
+      <form onSubmit={(e) => onFormSubmit(e)} className='ui form'>
+        <div className='field'>
+          <label>Video Search</label>
+          <input
+            type='text'
+            onChange={(e) => setSearchTerm(e.target.value)}
+            value={searchTerm}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
